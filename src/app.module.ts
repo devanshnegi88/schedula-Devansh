97a8717c9604e77da99e46ad6473b9d688b79662
Devansh <devanshnegi88@gmail.com>
2026-07-30 12:59:33 +0530

    fix: register AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DoctorModule } from './doctor/doctor.module';
@@ -44,5 +47,7 @@ import { AppointmentModule } from './appointment/appointment.module';
     CustomAvailabilityModule,
     AppointmentModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
